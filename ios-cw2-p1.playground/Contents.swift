85acb65d54bb00ae220f3d59cd79aecab82e411d
HaiderAlboloushi180 <halbolou05@gmail.com>
2020-06-17 21:02:07 +0300

    finished classwork

---
+++ ios-cw2-p1.playground/Contents.swift
@@ -0,0 +1,20 @@
+
+// CW #2
+let family = ["Haider", "Ali", "Bader", "Ziad"]
+
+var evens = [2, 4, 6, 8, 10, 12]
+
+var grades = [95.0, 93.5, 97.5]
+
+var trueFalse = [true, true, true, false]
+
+
+print(family[0], family[3])
+
+print(evens[0], evens[5])
+
+print(grades[0], grades[2])
+
+print(trueFalse[0], trueFalse[3])
+
+
---
+++ ios-cw2-p2.playground/Contents.swift
@@ -0,0 +1,31 @@
+//i luv pizza
+var fruits = ["mango", "orange", "potato", "peach"]
+
+
+print(fruits[0])
+
+print(fruits[3])
+
+
+fruits.append("strawberry")
+
+
+print(fruits)
+
+
+fruits.remove(at: 2)
+
+
+print(fruits)
+
+
+fruits += ["Banana", "Watermelon", "Pineapple", "Kiwi"]
+
+
+print(fruits)
+
+
+fruits.removeAll()
+
+
+print(fruits)
